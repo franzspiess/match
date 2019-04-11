ec18b1b6606f3b7c3e843f5752befdfc652cf29f
franzspiess <picnhepanchopincho@gmail.com>
2019-04-11 12:03:15 +0200

    Updates

--- precache-manifest.c023246c2df827cc45e55f2416a6a45f.js
+++ precache-manifest.c023246c2df827cc45e55f2416a6a45f.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/match/static/media/ball4.9f543099.png"
   },
   {
-    "revision": "28b99820cc5b2ad52e28",
+    "revision": "c2ebb73eaf853d2d43a6",
     "url": "/match/static/css/main.06ed0df0.chunk.css"
   },
   {
@@ -24,27 +24,27 @@ self.__precacheManifest = [
     "url": "/match/static/media/info2.8d4aff01.svg"
   },
   {
-    "revision": "28b99820cc5b2ad52e28",
-    "url": "/match/static/js/main.28b99820.chunk.js"
+    "revision": "c2ebb73eaf853d2d43a6",
+    "url": "/match/static/js/main.c2ebb73e.chunk.js"
   },
   {
     "revision": "91b10fb50ea37c7069ecf2fcdd93f5f0",
     "url": "/match/static/media/chat3.91b10fb5.svg"
   },
   {
-    "revision": "e56f70f89faba013eb18243dfb94ef53",
-    "url": "/match/static/media/cross3.e56f70f8.svg"
-  },
-  {
     "revision": "326664188389be9fc497c8bb3e994995",
     "url": "/match/static/media/tick3.32666418.svg"
+  },
+  {
+    "revision": "e56f70f89faba013eb18243dfb94ef53",
+    "url": "/match/static/media/cross3.e56f70f8.svg"
   },
   {
     "revision": "f1869b6044f1786104ef",
     "url": "/match/static/css/1.48d875d9.chunk.css"
   },
   {
-    "revision": "2c68b81f2a5a6ab33349f661d5c31d95",
+    "revision": "f6520da23330172669da3333b3bb5ffa",
     "url": "/match/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/match/precache-manifest.8a5ef4472cdd3088ddd070da8dd83b82.js"
+  "/match/precache-manifest.c023246c2df827cc45e55f2416a6a45f.js"
 );
 
 workbox.clientsClaim();
