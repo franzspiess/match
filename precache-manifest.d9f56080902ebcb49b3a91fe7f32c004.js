c004e9b78c00702a381abcdca8f81a6868398b9e
franzspiess <picnhepanchopincho@gmail.com>
2020-07-19 10:43:40 +0200

    Updates

---
+++ precache-manifest.d9f56080902ebcb49b3a91fe7f32c004.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c09aea8047072e8745c933cd2c40be",
+    "url": "/match/index.html"
+  },
+  {
+    "revision": "4d89aed739ef640e9b9e",
+    "url": "/match/static/css/2.9a70d772.chunk.css"
+  },
+  {
+    "revision": "7017fd8d4f5efa8c6954",
+    "url": "/match/static/css/main.cdefeeae.chunk.css"
+  },
+  {
+    "revision": "4d89aed739ef640e9b9e",
+    "url": "/match/static/js/2.4307a730.chunk.js"
+  },
+  {
+    "revision": "ac0abc3e6eff5a88b81f90a43946d65c",
+    "url": "/match/static/js/2.4307a730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7017fd8d4f5efa8c6954",
+    "url": "/match/static/js/main.5696a3f8.chunk.js"
+  },
+  {
+    "revision": "206af5c909197b3ae4ee",
+    "url": "/match/static/js/runtime-main.00f4449b.js"
+  },
+  {
+    "revision": "9f54309958785a8f6fb47bb3adc48a5d",
+    "url": "/match/static/media/ball4.9f543099.png"
+  },
+  {
+    "revision": "91b10fb50ea37c7069ecf2fcdd93f5f0",
+    "url": "/match/static/media/chat3.91b10fb5.svg"
+  },
+  {
+    "revision": "e56f70f89faba013eb18243dfb94ef53",
+    "url": "/match/static/media/cross3.e56f70f8.svg"
+  },
+  {
+    "revision": "8d4aff01ac73b55e48508392d213c2ce",
+    "url": "/match/static/media/info2.8d4aff01.svg"
+  },
+  {
+    "revision": "af8cbeaf0f8f606a26ed09114caa0e08",
+    "url": "/match/static/media/nadal2.af8cbeaf.jpg"
+  },
+  {
+    "revision": "326664188389be9fc497c8bb3e994995",
+    "url": "/match/static/media/tick3.32666418.svg"
+  }
+]);
